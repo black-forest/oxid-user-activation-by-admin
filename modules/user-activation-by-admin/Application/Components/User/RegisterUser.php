ce09dafb472a499c822a46b5b3ec8dc18bf8a676
Sven Baumann <baumann.sv@gmail.com>
2016-09-01 12:50:03 +0200

    deactivate user after confirm registration if isn´t active

--- modules/user-activation-by-admin/Application/Components/User/RegisterUser.php
+++ modules/user-activation-by-admin/Application/Components/User/RegisterUser.php
@@ -8,7 +8,6 @@
  * @license   GNU/LGPL
  * @copyright Copyright 2014-2016 BlackForest
  */
-
 class RegisterUser extends RegisterUser_parent
 {
     /**
@@ -23,5 +22,35 @@ class RegisterUser extends RegisterUser_parent
         }
 
         return parent::isEnabledPrivateSales();
+    }
+
+    /**
+     * Registration confirmation functionality. If registration
+     * succeded - redirects to success page, if not - returns
+     * exception informing about expired confirmation link
+     *
+     * @return mixed
+     */
+    public function confirmRegistration()
+    {
+        $user = oxNew('oxuser');
+
+        $deactivateUser = false;
+        if ($user->loadUserByUpdateId($this->getUpdateId())) {
+            $deactivateUser = $user->getFieldData('oxuser__oxactive') === '0';
+        }
+
+        $redirect = parent::confirmRegistration();
+
+        if ($deactivateUser) {
+            $updateUser = oxNew('oxuser');
+            $updateUser->load($user->getFieldData('oxuser__oxid'));
+            $updateUser->oxuser__oxactive = new oxField(0);
+            $updateUser->save();
+
+            $redirect = '';
+        }
+
+        return $redirect;
     }
 }
